1771c24de472b6891e80d2ea9fd442ddccc966b3
mauro <maurop123@gmail.com>
2017-10-25 13:06:28 -0400

    exclude node_modules

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -45,7 +45,8 @@ module.exports = {
       {
         test: /\.js$/,
         loader: 'babel-loader',
-        include: [resolve('src'), resolve('test')]
+        include: [resolve('src'), resolve('test')],
+        exclude: [resolve('node_modules')]
       },
       {
         test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
