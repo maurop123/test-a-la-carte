ceb7430fb3049903e59fc6f48dd86ea5d778f9e6
mauro <maurop123@gmail.com>
2017-10-25 12:54:12 -0400

    full setup

--- src/main.js
+++ src/main.js
@@ -3,7 +3,12 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-import {Vuetify, VApp, VButton} from 'vuetify'
+import {Vuetify, VApp} from 'vuetify'
+Vue.use(Vuetify, {
+  components: {
+    VApp,
+  }
+})
 
 Vue.config.productionTip = false
 
